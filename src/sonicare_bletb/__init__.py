b400171b73434a9cf0d22920f34479189faff746
Sander Schutten <schutten@hotmail.com>
2023-03-05 21:22:07 +0000

    fix

--- src/sonicare_bletb/__init__.py
+++ src/sonicare_bletb/__init__.py
@@ -1,34 +1,17 @@
-"""Parser for Sonicare BLE advertisements."""
 from __future__ import annotations
 
-from sensor_state_data import (
-    BinarySensorDeviceClass,
-    BinarySensorValue,
-    DeviceKey,
-    SensorDescription,
-    SensorDeviceClass,
-    SensorDeviceInfo,
-    SensorUpdate,
-    SensorValue,
-    Units,
-)
+__version__ = "0.2.0"
 
-from .parser import SonicareBinarySensor, SonicareBluetoothDeviceData, SonicareSensor
 
-__version__ = "0.0.2"
+from bleak_retry_connector import get_device
+
+from .exceptions import CharacteristicMissingError
+from .sonicare_bletb import BLEAK_EXCEPTIONS, SonicareBLETB, SonicareBLETBState
 
 __all__ = [
-    "SonicareSensor",
-    "SonicareBinarySensor",
-    "SonicareBluetoothDeviceData",
-    "BinarySensorDeviceClass",
-    "BinarySensorValue",
-    "SensorDescription",
-    "SensorDeviceInfo",
-    "DeviceKey",
-    "SensorUpdate",
-    "SensorDeviceClass",
-    "SensorDeviceInfo",
-    "SensorValue",
-    "Units",
+    "BLEAK_EXCEPTIONS",
+    "CharacteristicMissingError",
+    "SonicareBLETB",
+    "SonicareBLETBState",
+    "get_device",
 ]
